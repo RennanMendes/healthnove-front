52c4f75e807a9c97e23e6606c9ffa55823fc76a5
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2023-10-09 15:59:09 -0300

    feat: angular routes login and sign-up

--- src/app/components/sign-up/sign-up.component.ts
+++ src/app/components/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,12 +8,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private router: Router,
+  ) { }
 
   ngOnInit(): void {
   }
 
   entrar(){
+  
+  }
+
+  cancel(){
+
+    this.router.navigate(['/login']);
   
   }
 
