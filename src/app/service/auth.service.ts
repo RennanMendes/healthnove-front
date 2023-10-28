e9b1985724abcb1e6cd7abc1630d8486a938028e
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2023-10-27 21:18:23 -0300

    feat: form control group

--- src/app/service/auth.service.ts
+++ src/app/service/auth.service.ts
@@ -2,12 +2,15 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserRequestDto } from '../components/dto/UserRequestDto';
 import { Observable } from 'rxjs'
+import { environment } from 'src/environments/environment';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
+
+  private baseUrl: string = environment.baseUrl;
 
   constructor(
     private http: HttpClient
@@ -18,8 +21,13 @@ export class AuthService {
   }
 
   signUp(userDto: UserRequestDto): Observable<UserRequestDto> {
-    return this.http.post<UserRequestDto>("http://localhost:8080/users", userDto)
+    return this.http.post<UserRequestDto>(`${this.baseUrl}/users`, userDto)
   }
+
+  signIn(): Observable<UserRequestDto[]> {
+    return this.http.get<UserRequestDto[]>(`${this.baseUrl}/users`);
+  }
+
 
 
 
