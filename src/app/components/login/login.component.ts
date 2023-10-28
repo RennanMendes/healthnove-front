686313d929dcf8e52f6d5d5b27939c672c8df512
RennanMendes <95654544+RennanMendes@users.noreply.github.com>
2023-10-27 21:25:03 -0300

    Feature/login (#2)
    * feat: created login page
    
    * feat: form control group

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/service/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,13 +10,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm!: FormGroup;
+  email: string
+  password: string
+
+  user = {
+    email: '',
+    password: ''
+  };
+
+  constructor(
+    private router: Router,
+    private formBuilder: FormBuilder,
+    private authService: AuthService
+  ) { }
 
   ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      email: new FormControl (this.email, [Validators.required, Validators.email]),
+      password: new FormControl (this.password, Validators.required)
+    })
   }
 
   entrar() {
 
+    if (this.loginForm.valid) {
+       this.email = this.loginForm.value.email;
+       this.password = this.loginForm.value.password;
+
+      this.authService.signIn().subscribe({
+        next: (value) => {
+          console.log('Autenticado com sucesso', value)
+          this.router.navigateByUrl('/')
+          this.loginForm.reset();
+        },
+        error: (err) => {
+          console.log('Problema na autenticação', err)
+        },
+      })
+    }
+
+
   }
+
 
 }
